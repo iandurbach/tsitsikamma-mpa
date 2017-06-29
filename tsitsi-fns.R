faf1d156b251082df61e9fb1b0332aceee9eeda6
iandurbach <indurbach@gmail.com>
2017-06-29 22:38:03 +0200

    First commit

---
+++ tsitsi-fns.R
@@ -0,0 +1,115 @@
+#### calculates aggregated attribute values given a discount rate
+
+get_scores <- function(d = 0, dt0, dt5, dt10){
+  
+  # calculate time weights
+  tw <- (1-d)^c(0,5,10)
+  tw <- tw / sum(tw)
+  
+  # generate data aggregated over time
+  aggdata <- tw[1] * dt0 + tw[2] * dt5 + tw[3] * dt10
+  
+  return(aggdata)
+  
+}
+
+#### copies an attribute matrix N times to give SMAA input
+
+smaa_copy <- function(data,N){
+  newdata <- array(rep(as.matrix(data),each=N),c(N,nrow(data),ncol(data)))
+}
+
+#### takes (n * m) attribute matrix and SMAA wts, and returns plot of RAI's
+
+do_smaa <- function(meas,attnames,altnames){
+  pref <- simplex.sample(ncol(meas[1,,]), length(meas[,1,1]))$samples
+  result <- smaa(meas, pref)
+  
+  # plotting rank acceptability results
+  ras <- as.data.frame(unclass(result$ra))
+  names(ras) <- c("R1","R2","R3","R4","R5","R6","R7")
+  ras <- melt(cbind(ras, option = altnames), id.vars = c('option'))
+  ras$option <- factor(ras$option,levels=c("MPA-closed-R20M","MPA-closed","Open-1-add",
+                                           "Open-1","Open-3-add","Open-3","MPA-open"))
+  g1 <- ggplot(ras,aes(x = option, y = value,fill = variable)) + 
+    geom_bar(position = "fill",stat = "identity") +
+    scale_fill_brewer(direction = -1, palette = 1) +
+    theme_bw(base_size=24) +
+    theme(legend.text=element_text(size=20),
+          legend.title = element_blank(),
+          axis.text.x = element_text(angle = 90, hjust = 1, size = 20), 
+          axis.title = element_blank(),
+          legend.key.size = unit(2, 'lines'))
+  
+  # plotting central weight results
+  
+  cws <- as.data.frame(unclass(result$cw))
+  names(cws) <- attnames
+  
+  # aggregate some criteria for neater plot
+  cws_red <- data.frame(Environment = apply(cws[,1:6],1,sum),
+                        Economy = apply(cws[,7:11],1,sum),
+                        Equity = apply(cws[,12:16],1,sum),
+                        Political = apply(cws[,17:20],1,sum))
+  
+  # put full cws into long form
+  cws <- melt(cbind(cws, option = altnames), id.vars = c('option'))
+  
+  # reorder options
+  cws$option <- factor(cws$option,levels=c("MPA-closed-R20M","MPA-closed","Open-1-add",
+                                           "Open-1","Open-3-add","Open-3","MPA-open"))
+  # plot
+  g2 <- ggplot(cws,aes(x = option, y = value,fill = variable)) + 
+    geom_bar(position = "fill",stat = "identity") +
+    scale_fill_viridis(discrete = TRUE, direction = -1) + 
+    theme_bw(base_size=24) +
+    theme(legend.position = "bottom",
+          legend.text=element_text(size=20),
+          legend.title = element_blank(),
+          axis.text.x = element_text(angle = 90, hjust = 1, size = 20), 
+          axis.title = element_blank())
+  
+  # put reduced cws into long form
+  cws_red <- melt(cbind(cws_red, option = altnames), id.vars = c('option'))
+  
+  # reorder options
+  cws_red$option <- factor(cws_red$option,levels=c("MPA-closed-R20M","MPA-closed","Open-1-add",
+                                                   "Open-1","Open-3-add","Open-3","MPA-open"))
+  # plot
+  g3 <- ggplot(cws_red,aes(x = option, y = value,fill = variable)) + 
+    geom_bar(position = "fill",stat = "identity") + 
+    scale_fill_viridis(discrete = TRUE, direction = -1) + 
+    theme_bw(base_size=24) +
+    theme(legend.position = "bottom",
+          legend.text=element_text(size=20),
+          legend.title = element_blank(),
+          axis.text.x = element_text(angle = 90, hjust = 1, size = 20), 
+          axis.title = element_blank())
+  
+  return(list(ra = result$ra, cw = result$cw, 
+              plotra = g1, plotcw = g2, plotcw_red = g3))
+  
+}
+
+#### applies some uncertainty to attribute evaluations
+
+apply_uncertainty <- function(meas,alts,envu=c(0,0),ecou=c(0,0),socu=c(0,0),polu=c(0,0)){
+  
+  # environmental uncertainty
+  meas[,alts,1:6] <- meas[,alts,1:6] * runif(prod(dim(meas[,alts,1:6])), min = 1 - envu[1], max = 1 + envu[2])
+  meas[,,1:6] <- meas[,,1:6] / max(meas[,,1:6])
+  
+  # economic uncertainty  
+  meas[,alts,7:11] <- meas[,alts,7:11] * runif(prod(dim(meas[,alts,7:11])), min = 1 - ecou[1], max = 1 + ecou[2])
+  meas[,,7:11] <- meas[,,7:11] / max(meas[,,7:11])
+  
+  # equity (social) uncertainty
+  meas[,alts,12:16] <- meas[,alts,12:16] * runif(prod(dim(meas[,alts,12:16])), min = 1 - socu[1], max = 1 + socu[2])
+  meas[,,12:16] <- meas[,,12:16] / max(meas[,,12:16])
+  
+  # equity (political) uncertainty
+  meas[,alts,17:20] <- meas[,alts,17:20] * runif(prod(dim(meas[,alts,17:20])), min = 1 - polu[1], max = 1 + polu[2])
+  meas[,,17:20] <- meas[,,17:20] / max(meas[,,17:20])
+  
+  return(meas)
+}
---
+++ tsitsi-smaa.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(reshape2)
+library(hitandrun)
+library(smaa)
+library(viridis)
+
+load("tsitsi-data.RData")
+
+source("tsitsi-fns.R")
+    
+### analysis 1: just as is, no discounting
+
+# ranking at t_0
+scores_t0 <- (as.matrix(data_t0) %*% matrix(wts,ncol=1))
+sc_t0 <- scores_t0[order(scores_t0,decreasing = T),]
+
+# ranking at t_5
+scores_t5 <- (as.matrix(data_t5) %*% matrix(wts,ncol=1))
+sc_t5 <- scores_t5[order(scores_t5,decreasing = T),]
+
+# ranking at t_10
+scores_t10 <- (as.matrix(data_t10) %*% matrix(wts,ncol=1))
+sc_t10 <- scores_t10[order(scores_t10,decreasing = T),]
+
+# overall ranking depends on discount rate, first try no discounting
+ddata <- get_scores(d=0, dt0 = data_t0, dt5 = data_t5, dt10 = data_t10)
+scores <- (as.matrix(ddata) %*% matrix(wts,ncol=1))
+sc_all <- scores[order(scores,decreasing = T),]
+
+# heavy discounting won't change much, because similar rankings at each t
+ddata <- get_scores(d=0.06, dt0 = data_t0, dt5 = data_t5, dt10 = data_t10)
+scores <- (as.matrix(ddata) %*% matrix(wts,ncol=1))
+sc_d6 <- scores[order(scores,decreasing = T),]
+
+ddata <- get_scores(d=0.1, dt0 = data_t0, dt5 = data_t5, dt10 = data_t10)
+scores <- (as.matrix(ddata) %*% matrix(wts,ncol=1))
+sc_d10 <- scores[order(scores,decreasing = T),]
+
+res_knownwts <- cbind(sc_t0,sc_t5,sc_t10,sc_all,sc_d6,sc_d10)
+
+write.csv(res_knownwts,"results/scores-with-given-wts.csv")
+
+# weights are uncertain and stakeholder dependent, vary these with SMAA
+
+N <- 100000
+names.att <- names(data_t0)
+names.alt <- row.names(data_t0)
+
+meas <- smaa_copy(data_t0, N = N)
+smaa_t0 <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_t0.png", smaa_t0$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_t0.png", smaa_t0$plotcw_red, width=7, height=6.5, dpi=200)
+
+meas <- smaa_copy(data_t5, N = N)
+smaa_t5 <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_t5.png", smaa_t5$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_t5.png", smaa_t5$plotcw_red, width=7, height=6.5, dpi=200)
+
+meas <- smaa_copy(data_t10, N = N)
+smaa_t10 <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_t10.png", smaa_t10$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_t10.png", smaa_t10$plotcw_red, width=7, height=6.5, dpi=200)
+
+meas <- smaa_copy(get_scores(d = 0.05, dt0 = data_t0, dt5 = data_t5, dt10 = data_t10), N = N)
+smaa_tall <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_tall.png", smaa_tall$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_tall.png", smaa_tall$plotcw_red, width=7, height=6.5, dpi=200)
+
+# assessments of management scenarios also uncertain, test these
+
+# generic uncertainty, applied to all scenarios equally
+meas_t0u <- apply_uncertainty(meas = smaa_copy(data_t0, N = N), alts = 1:7, ecou = c(0.2,0.2), envu = c(0.2,0.2))
+meas_t5u <- apply_uncertainty(meas = smaa_copy(data_t5, N = N), alts = 1:7, ecou = c(0.4,0.4), envu = c(0.4,0.4))
+meas_t10u <- apply_uncertainty(meas = smaa_copy(data_t10, N = N), alts = 1:7, ecou = c(0.6,0.6), envu = c(0.6,0.6))
+
+meas <- get_scores(d=0.06, dt0 = meas_t0u, dt5 = meas_t5u, dt10 = meas_t10u)
+smaa_tall_wu <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_tall_wu.png", smaa_tall_wu$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_tall_wu.png", smaa_tall_wu$plotcw_red, width=7, height=6.5, dpi=200)
+rm(meas_t0u, meas_t5u, meas_t10u)
+
+# only downside uncertainty, applied to MPA closed scenarios
+meas_t0u <- apply_uncertainty(meas = smaa_copy(data_t0, N = N), alts = 1:2, ecou = c(0.2,0), envu = c(0.2,0))
+meas_t0u <- apply_uncertainty(meas = meas_t0u, alts = 3:7, ecou = c(0,0.2), envu = c(0,0.2))
+meas_t5u <- apply_uncertainty(meas = smaa_copy(data_t5, N = N), alts = 1:2, ecou = c(0.4,0), envu = c(0.4,0))
+meas_t5u <- apply_uncertainty(meas = meas_t5u, alts = 3:7, ecou = c(0,0.4), envu = c(0.4,0.4))
+meas_t10u <- apply_uncertainty(meas = smaa_copy(data_t10, N = N), alts = 1:2, ecou = c(0.6,0), envu = c(0.6,0))
+meas_t10u <- apply_uncertainty(meas = meas_t10u, alts = 3:7, ecou = c(0,0.6), envu = c(0,0.6))
+
+meas <- get_scores(d=0.06, dt0 = meas_t0u, dt5 = meas_t5u, dt10 = meas_t10u)
+smaa_tall_wu2 <- do_smaa(meas = meas, attnames = names.att, altnames = names.alt)
+ggsave("results/ai_tall_wu_antiMPA.png", smaa_tall_wu2$plotra, width=7, height=6.5, dpi=200)
+ggsave("results/cw_tall_wu_antiMPA.png", smaa_tall_wu2$plotcw_red, width=7, height=6.5, dpi=200)
+rm(meas, meas_t0u, meas_t5u, meas_t10u)
+
+#mydata_red <- data.frame(Env.Scarcity = apply(mydata[,1:3],1,sum),
+#                         Env.Risk = apply(mydata[,4:6],1,sum),
+#                         Eco = apply(mydata[,7:11],1,sum),
+#                         Eq.Soc.CR = mydata[,12],
+#                         Eq.Soc.PR = mydata[,13],
+#                         Eq.Soc.NR = apply(mydata[,14:16],1,sum),
+#                         Eq.PolGain = apply(mydata[,17:20],1,sum))
+
+
